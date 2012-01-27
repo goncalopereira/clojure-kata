71aac521695e1baab1761574b62313e6ce28cd08
Goncalo Pereira <goncalo.pereira@7digital.com>
2012-01-27 12:01:57 +0000

    flip to catch empty scenario so number is not != with array

--- src/myproject/core.clj
+++ src/myproject/core.clj
@@ -2,10 +2,10 @@
 
 (defn chop [number array] 
 	(loop [position 0 sub-array array]
-		(if (= number (first sub-array))
-			position
-			(if (= 0 (count sub-array))
-				-1
+		(if (= 0 (count sub-array))
+			-1
+			(if (= number (first sub-array))
+				position
 				(recur (+ position 1) (rest sub-array))
 			)
 		)
