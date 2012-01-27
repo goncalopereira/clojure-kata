7f31f9dde2dace549b39e9ec4a89fdd5864c7080
Goncalo Pereira <goncalo.pereira@7digital.com>
2012-01-27 16:20:19 +0000

    using map filter :-)

--- src/myproject/core.clj
+++ src/myproject/core.clj
@@ -1,8 +1,35 @@
 (ns myproject.core)
 
-(defn chop [number array]
-	(.indexOf array number)
-)	
+(defn check-position [position-value number]
+	(if (= number (last position-value))
+		(first position-value)
+		-1
+	)
+)
+
+(defn get-valid-positions [result-set-from-map]
+		(filter 
+			#(not= -1 %)
+			result-set-from-map
+		)
+)
+
+(defn chop [number array]	
+	(let [list-valid-positions	
+		(get-valid-positions
+				(map 
+					#(check-position % number) 
+					(map-indexed vector array)	
+				)
+			)
+		]
+	
+		(if (empty? list-valid-positions)
+			-1
+			(first list-valid-positions)
+		)
+	)	
+)
 
 
 
