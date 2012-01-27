0adf861ede9615f333b631cda91a429629adb088
Goncalo Pereira <goncalo.pereira@7digital.com>
2012-01-27 11:01:23 +0000

    param 2 is right on size 4

--- test/myproject/test/core.clj
+++ test/myproject/test/core.clj
@@ -16,7 +16,7 @@
  
 (deftest size-four-array-right-param-on-0-is-0 (is (=  0 (chop 1 [1 3 5 7]) )))
 (deftest size-four-array-right-param-on-1-is-1  (is (=  1 (chop 3 [1 3 5 7]) )))
-;(deftest size-four-array-right-param-on-2-is-2  (is (=  2 (chop 5 [1 3 5 7]) )))
+(deftest size-four-array-right-param-on-2-is-2  (is (=  2 (chop 5 [1 3 5 7]) )))
 ;(deftest size-four-array-right-param-on-3-is-3  (is (=  3 (chop 7 [1 3 5 7]) )))
 ;(deftest size-four-array-wrong-param-0-is-false  (is (= -1 (chop 0 [1 3 5 7]) )))
 ;(deftest size-four-array-wrong-param-2-is-false  (is (= -1 (chop 2 [1 3 5 7]) )))
