3bc5b6d287fb7333ddb82cfea0538bef5d668e17
Goncalo Pereira <goncalo.pereira@7digital.com>
2012-01-27 11:59:53 +0000

    doesn't need fn anymore

--- src/myproject/core.clj
+++ src/myproject/core.clj
@@ -1,12 +1,8 @@
 (ns myproject.core)
 
-(defn first-position-is-correct [number array]
-		(= number (first array))	
-)
-
 (defn chop [number array] 
 	(loop [position 0 sub-array array]
-		(if (first-position-is-correct number sub-array)
+		(if (= number (first sub-array))
 			position
 			(if (= 0 (count sub-array))
 				-1
