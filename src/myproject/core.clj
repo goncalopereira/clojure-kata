ab6fa7313c472014ce4bd31fe1347fa14a21c9c9
Goncalo Pereira <goncalo.pereira@7digital.com>
2012-01-27 12:13:16 +0000

    OK if it's impossible to be there then remove it from the start, down to 33

--- src/myproject/core.clj
+++ src/myproject/core.clj
@@ -1,16 +1,19 @@
 (ns myproject.core)
 
-(defn chop [number array] 
-	(loop [position 0 sub-array array]
-		(do (println position sub-array)
-		(if (or (= 0 (count sub-array)) (< number (first sub-array)))
-			-1
-			(if (= number (first sub-array))
-				position
-				(recur (+ position 1) (rest sub-array))
-			)
-		))
-	)
+(defn chop [number array]
+	(if (or (nil? (last array)) (< (last array) number))
+		-1 
+		(loop [position 0 sub-array array]
+			(do (println position sub-array)
+			(if (or (= 0 (count sub-array)) (< number (first sub-array)))
+				-1
+				(if (= number (first sub-array))
+					position
+					(recur (+ position 1) (rest sub-array))
+				)
+			))
+		)
+	)	
 )
 
 	
