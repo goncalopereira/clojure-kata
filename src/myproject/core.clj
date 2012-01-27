099a569e175ce5f7f5aa777c0e39a54687b6d599
Goncalo Pereira <goncalo.pereira@7digital.com>
2012-01-27 14:14:03 +0000

    or we can just use the build-in function ;)

--- src/myproject/core.clj
+++ src/myproject/core.clj
@@ -1,17 +1,8 @@
 (ns myproject.core)
 
-(defn number-is-out-of-bounds [number array]
-	(or (empty? array) (< number (first array)) (> number (last array)))
-)
-
 (defn chop [number array]
-	(loop [position 0 sub-array array]
-		(if (number-is-out-of-bounds number sub-array)
-			-1
-			(if (= number (first sub-array))
-				position
-				(recur (+ position 1) (rest sub-array))
-			)
-		)
-	)
+	(.indexOf array number)
 )	
+
+
+
