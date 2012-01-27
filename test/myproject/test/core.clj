1110573a2102cd0308ca1172d6518ad5fe176700
Goncalo Pereira <goncalo.pereira@7digital.com>
2012-01-27 10:58:31 +0000

    fail on 2nd param on test with size 3 array is caught

--- test/myproject/test/core.clj
+++ test/myproject/test/core.clj
@@ -10,7 +10,7 @@
 (deftest size-three-array-right-param-on-1-is-1 (is (=  1 (chop 3 [1 3 5]) )))
 (deftest size-three-array-right-param-on-2-is-2  (is (=  2 (chop 5 [1 3 5]) )))
 (deftest size-three-array-wrong-param--0-is-false  (is (= -1 (chop 0 [1 3 5]) )))
-;(deftest size-three-array-wrong-param-2-is-false (is (= -1 (chop 2 [1 3 5]) ))
+(deftest size-three-array-wrong-param-2-is-false (is (= -1 (chop 2 [1 3 5]) )))
 ;(deftest size-three-array-wrong-param-4-is-false  (is (= -1 (chop 4 [1 3 5]) )))
 ;(deftest size-three-array-wrong-param-6-is-false  (is (= -1 (chop 6 [1 3 5]) )))
  
