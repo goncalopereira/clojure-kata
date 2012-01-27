c2789677f54225099fa8736bba978a3d5c627943
Goncalo Pereira <goncalo.pereira@7digital.com>
2012-01-27 13:52:00 +0000

    remove println

--- src/myproject/core.clj
+++ src/myproject/core.clj
@@ -6,13 +6,12 @@
 
 (defn chop [number array]
 	(loop [position 0 sub-array array]
-		(do (println position sub-array)
 		(if (number-is-out-of-bounds number sub-array)
 			-1
 			(if (= number (first sub-array))
 				position
 				(recur (+ position 1) (rest sub-array))
 			)
-		))
+		)
 	)
 )	
